e7b79508f1c0b49b7e1bf1eb32fbbc35cedbb8ae
akakib <akakib.ctg@gmail.com>
2021-10-11 17:05:25 +0600

    about us

--- resources/views/landing/about_us.blade.php
+++ resources/views/landing/about_us.blade.php
@@ -5,60 +5,57 @@
             <div class="row">
                 <div class="col-md-6 py-5">
                     <div class="pl-3">
-                        <h3 class="text-purple text-lg font-bebas">OUR EXAMS</h3>
-                        <p class="fw-600 text-xsm max-w-38 w-100 mx-auto text-purple-half">An exam or examination is an official test that shows your knowledge or ability in a particular subject</p>
+                        <h3 class="text-purple text-lg font-bebas">WHO WE ARE</h3>
+                        <p class="fw-600 text-xsm max-w-38 w-100 mx-auto text-purple-half">A group of passionate educators striving to make 
+                            education easy, fun, and personal with the help of technology. We want to increase the accessibility of quality
+                             education while empowering learners, parents, and teachers alike.
+                        </p>
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <img src="/img/landing/top_banner.png" class="img-fluid" alt="">
+                    <img src="/img/about/aboutus.png" class="img-fluid" alt="">
                 </div>
             </div>
         </div>
     </section> <!--header banner end-->
+    <div class="container-fluid">
+        <img src="/img/about/process.png" class="img-fluid" alt="">
+    </div>
 
     <div class="our-exams-section text-center py-5">
         <div class="container">
-            <h3 class="text-purple text-lg font-bebas">OUR EXAMS</h3>
-            <p class="fw-600 text-xsm max-w-38 w-100 mx-auto text-purple-half">An exam or examination is an official test that shows your knowledge or ability in a particular subject</p>
-            <a href="#" class="btn text-xsm text-white bg-purple px-4">College</a>
+            <h3 class="text-purple text-lg font-bebas">THE EDVENTURE MANIFESTO</h3>
+            <p class="fw-600 text-xsm max-w-38 w-100 mx-auto text-purple-half">A group of passionate educators striving to make education easy, 
+                fun, and personal with the help of technology.</p>
             <div class="row my-5">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="single-exam p-4">
-                        <img src="/img/landing/exam_1.png" width="50" alt="">
-                        <h5 class="text-sm fw-800 mt-3">Physics</h5>
-                        <p class="text-xxsm fw-400 mt-3">Lorem Ipsum is simply dummy text of the printing</p>
-                        <a href="#" class="btn btn-outline text-purple mt-4">Go To Exam</a>
+                        <img src="/img/about/pa.png"  alt="">
+                        <h5 class="text-sm fw-800 mt-3">Personal Assistance</h5>
+                        <p class="text-xxsm fw-400 mt-3">We focus on personalized assistance over personalized education</p>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="single-exam p-4">
-                        <img src="/img/landing/exam_2.png" width="50" alt="">
-                        <h5 class="text-sm mt-3">Chemistry</h5>
-                        <p class="text-xxsm fw-400 mt-3">Lorem Ipsum is simply dummy text of the printing</p>
-                        <a href="#" class="btn btn-outline text-purple mt-4">Go To Exam</a>
+                        <img src="/img/about/sc.png"  alt="">
+                        <h5 class="text-sm mt-3">Seamless communication</h5>
+                        <p class="text-xxsm fw-400 mt-3">Ensuring seamless communication between students and teachers is more important to us than sticking to one platform or device</p>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="single-exam p-4">
-                        <img src="/img/landing/exam_3.png" width="50" alt="">
-                        <h5 class="text-sm mt-3">Math</h5>
-                        <p class="text-xxsm fw-400 mt-3">Lorem Ipsum is simply dummy text of the printing</p>
-                        <a href="#" class="btn btn-outline text-purple mt-4">Go To Exam</a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="single-exam p-4">
-                        <img src="/img/landing/exam_4.png" width="50" alt="">
-                        <h5 class="text-sm mt-3">Biology</h5>
-                        <p class="text-xxsm fw-400 mt-3">Lorem Ipsum is simply dummy text of the printing</p>
-                        <a href="#" class="btn btn-outline text-purple mt-4">Go To Exam</a>
+                        <img src="/img/about/te.png"  alt="">
+                        <h5 class="text-sm mt-3">Time Efficiency</h5>
+                        <p class="text-xxsm fw-400 mt-3">Following a rigid schedule for the sake of routine seems useless to us without the best possible use of the time</p>
                     </div>
                 </div>
             </div>
-
-            <a href="#" class="btn text-xxsm text-white bg-purple px-4 py-2">View More Courses</a>
         </div>
-    </div> <!-- our exam section end -->
+    </div> <!-- manifesto section end -->
+
+    <div class="container-fluid">
+        <img src="/img/about/ecosystem.png" class="img-fluid" alt="">
+    </div>
 
     <div class="our-exams-section text-center py-5">
         <div class="container">
@@ -66,128 +63,15 @@
             <div class="row my-5">
                 <div class="col-md-3">
                     <div class="single-exam p-4">
-                        <h5 class="text-sm fw-800 mt-3">Physics</h5>
-                        <p class="text-xxsm fw-400 mt-3">Lorem Ipsum is simply dummy text of the printing</p>
+                        <img src="/img/about/ceo.png" class="img-fluid"  alt="">
                     </div>
                 </div>
                 
             </div>
             
         </div>
-    </div> <!-- our exam section end -->
+    </div> <!-- ceo section end -->
 
-    <div class="our-package-section text-center py-5">
-        <div class="container">
-            <h3 class="text-purple text-lg font-bebas">OUR PACKAGE</h3>
-            <p class="fw-600 text-xsm max-w-38 w-100 mx-auto text-purple-half">Each one of our packages has been designed as a base upon which we build your customized collaboration.</p>
-            <div class="single-package bg-art py-5 px-3 mt-6">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="ml-4">
-                            <div class="package-icon text-left"><img src="/img/landing/exam_icon.png" class="img-fluid" alt="exam" /></div>
-                            <h4 class="text-xmd text-left mt-3">অনলাইন পরীক্ষা</h4>
-                            <p class="text-sm fw-400 text-gray text-left">SpeedExam is the best onlineinstitution to conduct assessments and quizzes with great features and effective analytical reports.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="/img/landing/chemistry.png" class="drop-shadow img-fluid" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="single-package bg-art py-5 px-3 mt-6">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="ml-4">
-                        <div class="package-icon text-left"><img src="/img/landing/dashboard_icon.png" class="img-fluid" alt="Result" /></div>
-                        <h4 class="text-xmd text-left mt-3">Personal Dashboard</h4>
-                        <p class="text-sm fw-400 text-gray text-left mt-3">Personal dashboards simplify business communications, offering employees a tailored view that helps them stay on top of their communications with minimal unnecessary distraction.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="/img/landing/dashboard.png" class="drop-shadow img-fluid" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="single-package bg-art py-5 px-3 mt-6">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="ml-4">
-                        <div class="package-icon text-left"><img src="/img/landing/result_icon.png" class="img-fluid" alt="Result" /></div>
-                        <h4 class="text-xmd text-left mt-3">Comparative Analysis</h4>
-                        <p class="text-sm fw-400 text-gray text-left mt-3">Comparative studies are the studies to demonstrate ability to examine, compare and contrast subjects or ideas. Comparative study shows how two subjects are similar or shows how two subjects are different.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="/img/landing/result.png" class="drop-shadow img-fluid" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="single-package bg-art py-5 px-3 mt-6">
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="/img/landing/solution.png" class=" img-fluid" alt="">
-                    </div>
-                    <div class="col-md-6">
-                        <div class="ml-4">
-                        <div class="package-icon text-left"><img src="/img/landing/video_icon.png" class="img-fluid" alt="video" /></div>
-                        <h4 class="text-xmd text-left mt-3">Solution Class</h4>
-                        <p class="text-sm fw-400 text-gray text-left mt-3">NCTB Textbook Solution National Curriculum and Textbook Board Bangladesh Class 1 to 10 Textbooks Solution for Mathematics, Science & More.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div><!--our package section end-->
-
-    <div class="our-exams-section text-center py-5">
-        <div class="container">
-            <h3 class="text-purple text-lg font-bebas">Featured Videos</h3>
-            <p class="fw-600 text-xsm max-w-38 w-100 mx-auto text-purple-half">A few simple steps separate you from your 
-                upcoming journey. See how easy it is to learn.  </p>
-            @include('landing.videos')
-        </div>
-    </div>
-
-    <div class="py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <h3 class="text-purple text-lg font-bebas">Mother Earth Hosts Our Reader</h3>
-                    <p class="fw-600 text-xsm max-w-38 w-100 mx-auto text-purple-half">Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry. Lorem Ipsum has been the industry's standard dummy text ever
-                        since the 1500s, when an unknown printer.</p>
-                    <a href="#" class="btn text-xxsm text-white bg-purple px-4 py-2">BUY THE BOOK</a>
-                </div>
-                <div class="col-md-8">
-                    <div class="row mt-5 mt-md-0 align-items-center">
-                        <div class="col-5">
-                            <div class="single-reader">
-                                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/vQkXF5__TQI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                        <div class="col-7">
-                            <div class="single-reader">
-                                <iframe class="h-18" width="100%" height="auto" src="https://www.youtube.com/embed/AYDmsaiWY9U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row align-items-center">
-                        <div class="col-2"></div>
-                        <div class="col-4">
-                            <div class="single-reader">
-                                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/vQkXF5__TQI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="single-reader">
-                                <iframe class="h-18" width="100%" height="auto" src="https://www.youtube.com/embed/AYDmsaiWY9U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div><!-- our reader section end -->
     @include('landing.footer')
 </x-guest-layout>
 
