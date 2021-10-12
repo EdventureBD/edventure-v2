0886d3993e97b070410bb8505c8a7ec05d828b61
Ajit Das <ajitdas2900@gmail.com>
2021-10-12 17:28:36 +0600

    author section fixed

--- resources/views/landing/about_us.blade.php
+++ resources/views/landing/about_us.blade.php
@@ -61,17 +61,19 @@
         <div class="container">
             
             <div class="row my-5">
-                <div class="col-md-3">
-                    <div class="single-author">
-                        <img src="/img/about/shahriar.png" class="img-fluid"  alt="">
-                        <div class="social-icons d-flex justify-content-between">
+                <div class="col-md-4">
+                    <div class="single-author p-3">
+                        <img src="/img/about/shahriar.png" class="w-15 mb-3 img-fluid"  alt="">
+                        <h3 class="text-xxmd text-white">Md. Shahriar Iftekhar</h3>
+                        <p class="text-sm text-white">CEO</p>
+                        <div class="mt-3 mx-4 social-icons d-flex justify-content-between">
                             <a href="#"><img src="/img/about/facebook.png" class="img-fluid"  alt=""></a>
                             <a href="#"><img src="/img/about/instagram.png" class="img-fluid"  alt=""></a>
                             <a href="#"><img src="/img/about/twitter.png" class="img-fluid"  alt=""></a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-9 pl-3 pl-md-5">
+                <div class="col-md-8 pl-3 pl-md-5">
                     <h3 class="text-purple text-lg font-bebas">MESSAGE FROM THE CEO</h3>
                     <p class="fw-600 text-xsm w-100 text-purple-half">Growing up in a country with the highest population density and inadequate resources often leads me to think of alternative solutions to existing problems. The education system of Bangladesh is one of the prime sufferers of the population and resource problem we have in general. Online learning, in this context, comes with a lot of opportunities to be explored. However, as though the term “online learning” sounds really promising in trying to solve the resource problem we have, finding out what works out and what doesn’t in an online learning module is really critical. We, in Edventure, believe online learning to be a facilitator of the existing learning eco-system, not a replacement. Learning is an inter-connected process comprising the student, teacher and guardian where we believe spontaneous participation of every stakeholder is extremely important. Our primary mission is to build an eco-system where all the stakeholders are empowered to spontaneously participate in the learning journey by enabling them with necessary resources and relevant modern technologies. Edventure, in a nutshell, is a group of passionate educators and tech professionals who strive to translate their experiences as both student and teacher into the service we are going to offer in order to make education in general easy, fun and personal”                        
                     </p>
