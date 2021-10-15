dfa825591e30bf92ff889ff2de2ea005e755f99c
DESKTOP-DU0UBID\Top5Way <akakib.ctg@gmail.com>
2021-10-15 10:13:34 +0600

    login ui

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
             <div>
                 <x-label for="name" :value="__('Name')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                <x-input id="name" placeholder="e.g. Fahim khan ridoy" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
                     autofocus />
             </div>
 
@@ -24,21 +24,21 @@
             <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-input placeholder="e.g. yourmail@test.com" id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
             <div>
                 <x-label for="phone" :value="__('Phone')" />
 
-                <x-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required
+                <x-input placeholder="e.g. 016xxxxxxxx" id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required
                     autofocus />
             </div>
 
             <!-- Password -->
             {{-- <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label  for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                <x-input  id="password" class="block mt-1 w-full" type="password" name="password" required
                     autocomplete="new-password" />
             </div> --}}
 
@@ -50,7 +50,7 @@
                         id="password" name="password" required autocomplete="current-password">
                     <div class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
 
-                        <svg class="h-5 text-gray-700" fill="none" @click="show = !show"
+                        <!-- <svg class="h-5 text-gray-700" fill="none" @click="show = !show"
                             :class="{'hidden': !show, 'block':show }" xmlns="http://www.w3.org/2000/svg"
                             viewbox="0 0 576 512">
                             <path fill="currentColor"
@@ -64,7 +64,7 @@
                             <path fill="currentColor"
                                 d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z">
                             </path>
-                        </svg>
+                        </svg> -->
                     </div>
                 </div>
             </div>
@@ -85,7 +85,7 @@
                         id="password_confirmation" name="password_confirmation" required
                         autocomplete="current-password">
                     <div class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-
+<!-- 
                         <svg class="h-5 text-gray-700" fill="none" @click="show = !show"
                             :class="{'hidden': !show, 'block':show }" xmlns="http://www.w3.org/2000/svg"
                             viewbox="0 0 576 512">
@@ -100,7 +100,7 @@
                             <path fill="currentColor"
                                 d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z">
                             </path>
-                        </svg>
+                        </svg> -->
                     </div>
                 </div>
             </div>
