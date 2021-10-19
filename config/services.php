dbf16b96cc597b74dfbe489e7d1a3829f8c9fcee
Ajit Das <ajitdas2900@gmail.com>
2021-10-19 10:49:17 +0600

    services.php updated

--- config/services.php
+++ config/services.php
@@ -32,11 +32,11 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => url('login/google/callback'),
+        'redirect' => 'login/google/callback',
     ],
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => url('login/facebook/callback'),
+        'redirect' => 'login/facebook/callback',
     ],
 ];
