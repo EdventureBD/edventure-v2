6d969c76f73203109ea2a958958120a42168adfe
Ajit Das <ajitdas2900@gmail.com>
2021-10-26 15:47:49 +0600

    edvanture otp registration updated

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use GuzzleHttp\Client;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
 
 class RegisteredUserController extends Controller
@@ -32,31 +34,86 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate(
-            [
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|email|max:255|unique:users',
-                'phone' => 'required|numeric|digits:11|unique:users',
-                // 'verificationId' => 'required|min:200',
-                'password' => 'required|string|confirmed|min:8',
-            ]
-            // , [
-            //     'verificationId.required' => 'Phone number verification failed!'
-            // ]
-        );
 
-        Auth::login($user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'password' => Hash::make($request->password),
-            'is_admin' => 0,
-            'user_type' => 3,
-            'image' => null,
-        ]));
+        if (empty(request()->otp)) {
+            $request->validate(
+                [
+                    'name' => 'required|string|max:255',
+                    'email' => 'required|string|email|max:255|unique:users',
+                    'phone' => 'required|numeric|digits:11|unique:users',
+                    // 'verificationId' => 'required|min:200',
+                    'password' => 'required|string|confirmed|min:8',
+                ]
+                // , [
+                //     'verificationId.required' => 'Phone number verification failed!'
+                // ]
+            );
+            $input = $request->all();
+            return $this->sendOtp($input);
+        } else if (!empty(request()->otp)) {
+            if (Cache::get(request()->ip . '_otp') == request()->otp) {
+                $input = Cache::get(request()->ip . '_input');
+                // dd($ainput);
+                Auth::login($user = User::create([
+                    'name' => $input['name'],
+                    'email' => $input['email'],
+                    'phone' => $input['phone'],
+                    'password' => Hash::make($input['password']),
+                    'is_admin' => 0,
+                    'user_type' => 3,
+                    'image' => null,
+                ]));
+                Cache::forget(request()->ip . '_otp');
+                Cache::forget(request()->ip . '_input');
+                event(new Registered($user));
 
-        event(new Registered($user));
+                return redirect(RouteServiceProvider::StudentHOME);
+            }
+        }
 
-        return redirect(RouteServiceProvider::StudentHOME);
+
+        // Auth::login($user = User::create([
+        //     'name' => $input['name'],
+        //     'email' => $input['email'],
+        //     'phone' => $input['phone'],
+        //     'password' => Hash::make($input['password']),
+        //     'is_admin' => 0,
+        //     'user_type' => 3,
+        //     'image' => null,
+        // ]));
+
+        // event(new Registered($user));
+
+        // return redirect(RouteServiceProvider::StudentHOME);
+    }
+
+    public function sendOtp($input)
+    {
+        $otp = mt_rand(100000, 999999);
+        Cache::put(request()->ip . '_otp', $otp, 120);
+        Cache::put(request()->ip . '_input', $input, 120);
+        $message = "Your Edventure register otp is " . $otp;
+        $client = new Client();
+        $api_key = config("edv.reve_api_key");
+        $api_secret = config("edv.reve_secret_key");
+        $caller_id = config("edv.reve_caller_id");
+        $url = "https://smpp.ajuratech.com:7790/sendtext?apikey=" . $api_key . "&secretkey=" . $api_secret . "&callerID=" . $caller_id . "&toUser=" . $input['phone'] . "&messageContent=" . $message;
+        // dd($url);
+        try {
+            $result = $client->request('GET', $url);
+            $result = $result->getBody()->getContents();
+            $result = json_decode($result, true);
+            // dd($result);
+            if ($result['Status'] == "0") {
+                return view("auth.otp");
+            }
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            // catches all ClientExceptions
+            return view('auth.register');
+            // dd($e);
+        } catch (\GuzzleHttp\Exception\ConnectException $e) {
+            // dd($e);
+            return view('auth.register');
+        }
     }
 }
---
+++ config/edv.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'reve_api_key' => env('REVE_API_KEY'),
+    'reve_secret_key' => env('REVE_SECRET_KEY'),
+    'reve_caller_id' => env('REVE_CALLER_ID'),
+
+];
