57e3febe842abc044c1a125c5951ede07ce12ac8
hamid <hamidlive6@gmail.com>
2021-10-29 18:36:17 +0600

    contact

---
+++ resources/views/landing/contact_us.blade.php
@@ -0,0 +1,100 @@
+<x-landing-layout>
+
+    <section class="header-banner pt-7">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 py-5">
+                    <div class="pl-3">
+                        <h3 class="text-purple text-lg fw-400 font-bebas">CONTACT</h3>
+                        <p class="fw-600 text-xsm max-w-38 w-100 text-purple-half">A group of passionate educators striving to make education easy, fun, and personal with the help of technology. We want to increase the accessibility of quality education while empowering learners, parents, and teachers alike.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="/img/about/aboutus.png" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </section> <!--header banner end-->
+
+<!--Contact form -->
+<div class="container-fluid">
+    <div class="row">
+
+        <div class="shape-a">
+            <div class="form-box">
+                <h3>We’d love to hear from <span>You</span></h3>
+                
+                <form action="#" class="form-group">
+                    <label for="name">name</label>
+                    <br>
+                    <input type="text" id="name"/>
+
+                    <br>
+                    
+                    <label for="e-mail">e-mail</label>
+                    <br>
+                    <input type="email"  id="e-mail">
+
+                    <br>
+
+                    <label for="message">Message</label>
+                    <br>
+                    <input type="text" id="message"/>
+                    <input type="text" id="message"/>
+                    <br>
+                    
+                    <button type="submit" data-target="#" class="btn-submit">Submit</button>
+                </form>
+
+            </div>
+        </div>
+
+        <!--address box-->
+
+        <div class="shape-b">
+            <div class="contact-box">
+                
+                <div class="text-box">
+
+                    <h3>Contact <span>Information</span> </h3>
+                    <p>117, Kazi Nazrul Islam<br>
+                    Avenue 1000 Dhaka, Dhaka
+                    <br>Division, Bangladesh</p>
+                    <p>Call us: +8801746483678<br>
+                    We are open from Sunday - Thursday:
+                    <br>
+                    10:00 AM- 7:00 PM 
+                    <br>
+                    E-mail: business@edventurebd.com</p>
+                    <h4>Follow us</h4>
+
+                    <div class="icon-box">
+                        
+                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-facebook-official allign" aria-hidden="true"></i></i></a>
+                    </div>
+
+                </div>
+                
+            </div>
+
+            
+        </div>
+    </div>
+</div>
+
+<!--map-->
+<div class="map-container">
+    <div class="map">
+        <img src="img/contact/image3.png" alt="map" width="100%" class="img-fluid" >
+        <img src="img/contact/imageopt_2.png" alt="map" width="50%" class="img-fluid building">
+    </div>
+</div>
+
+
+    
+</x-landing-layout>
+
+
