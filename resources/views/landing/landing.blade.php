f2a2a1676f845fe9043e51a9fc3c682bc759912f
Ajit Das <ajitdas2900@gmail.com>
2021-11-11 16:36:40 +0600

    dskds

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -32,7 +32,7 @@
             <a href="#" class="btn fw-800 text-xxsm text-white bg-purple px-4">HSC</a>
             <div class="row justify-content-center my-5 ">
                 @foreach ($courses as $course)
-                <div class="col-md-4 col-lg-3">
+                <div class="col-md-4 col-lg-3 mb-3">
                     <div class="single-exam mx-auto p-4 mb-4 mb-md-0" style="background-image: url({{asset($course->banner)}}); " >
                         <img src="{{asset($course->icon)}}" width="50" alt="">
                         <h5 class="text-sm mt-3">{{$course->title}}</h5>
