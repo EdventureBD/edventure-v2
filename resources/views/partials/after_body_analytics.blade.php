215fb28f44036f8453a141a7dcfffb93325965c5
akakib <akakib.ctg@gmail.com>
2021-11-13 14:22:49 +0600

    bug fix

--- resources/views/partials/after_body_analytics.blade.php
+++ resources/views/partials/after_body_analytics.blade.php
@@ -1,5 +1,5 @@
 @if(config('app.url') == 'https://edventurebd.com')
-<!-- Start of HubSpot Embed Code -->
-<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/20924540.js"></script>
-<!-- End of HubSpot Embed Code -->
-@endphp
+    <!-- Start of HubSpot Embed Code -->
+    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/20924540.js"></script>
+    <!-- End of HubSpot Embed Code -->
+@endif
