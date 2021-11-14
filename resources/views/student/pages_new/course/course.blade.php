671bb8e2bd5a2ddcd23533fc3211325cd54bb5b1
akakib <akakib.ctg@gmail.com>
2021-11-14 17:20:01 +0600

    courses page

--- resources/views/student/pages_new/course/course.blade.php
+++ resources/views/student/pages_new/course/course.blade.php
@@ -8,8 +8,8 @@
             <div class="row card-group-row mb-lg-8pt">
                 @foreach ($courses as $course)
                 <div class="col-md-3 mb-4">
-                    <div class="single-exam mx-auto p-4 mb-md-0" style="background-image: url('/img/landing/physics.png')">
-                        <img src="/img/landing/exam_1.png" width="50" alt="">
+                    <div class="single-exam mx-auto p-4 mb-md-0" style="background-image: url({{asset($course->banner)}});">
+                        <img src="{{asset($course->icon)}}" width="50" alt="">
                         <h5 class="text-sm mt-3">{{ $course->title }} </h5>
                         {{-- <p class="text-xxsm fw-400 mt-3">Course details</p> --}}
                         <a href="{{ route('course-preview', $course->slug) }}"  class="btn btn-outline text-purple mt-4">Go To Exam</a>
