4581f5f0b057a0e23acdcb57a5a50bb66775483b
EdventureBD <business@edventurebd.com>
2022-01-11 16:39:46 +0600

    initial commit for roadmap

--- resources/views/student/pages_new/roadmap/roadmap_index.blade.php
+++ resources/views/student/pages_new/roadmap/roadmap_index.blade.php
@@ -6,6 +6,33 @@
          </div>
          <div class="row justify-content-center all-blog py-3 card-group-row mb-lg-8pt">
             <h1> Insert Code Here </h1>
+            <div class="container w-100 my-5" id="roadmapParentContainer">
+               <div class="" id="leftLands">
+                  <img src="" alt="">
+                  <img src="/img/road_map/landl1.png" alt="leftIland" id="ilands">
+               </div>
+               <div class="" id="arrows">
+                  <img src="/img/road_map/arrowL2R.png" alt="L2Rarrows" class="img-fluid">
+                  <img src="/img/road_map/arrowR2L.png" alt="R2Larrows" class="img-fluid">
+               </div>
+               <div class="" id="rightIland">
+                  <img src="" alt="">
+                  <img src="/img/road_map/landr1.png" alt="">
+               </div>
+               <div class="d-flex" id="leftLands">
+                  <img src="" alt="">
+                  <img src="/img/road_map/landr1.png" alt="leftIland">
+               </div>
+               <div class="align-bottom" id="arrows">
+                  <img src="/img/road_map/arrowL2R.png" alt="L2Rarrows" class="img-fluid">
+                  <img src="/img/road_map/arrowR2L.png" alt="R2Larrows" class="img-fluid">
+               </div>
+               <div class="" id="rightIland">
+                  <img src="" alt="">
+                  <img src="/img/road_map/landr1.png" alt="">
+               </div>
+
+            </div>
          </div>
       </div>
 </div>
