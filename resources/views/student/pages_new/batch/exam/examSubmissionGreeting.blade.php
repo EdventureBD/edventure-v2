273763a432b3b2e2ee48cff29c299244e0a7813b
EdventureBD <business@edventurebd.com>
2022-01-19 12:48:11 +0600

    css bug fixed for contact_us page

--- resources/views/student/pages_new/batch/exam/examSubmissionGreeting.blade.php
+++ resources/views/student/pages_new/batch/exam/examSubmissionGreeting.blade.php
@@ -3,7 +3,9 @@
        <div class="my-5 py-5 d-flex justify-content-center">
             <div id="successfully_submitted_section">
                 <h1>Thank you for participating in the exam.</h1>
-                <h1>Your script has been submitted successfully!</h1>
+                <div class="d-flex">
+                    <h5 class="text-danger">Your script has been submitted successfully!</h5>
+                </div>
             </div>
        </div>
 
