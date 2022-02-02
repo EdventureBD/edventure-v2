7601b767d1fbbca7e32f284bec257a06ec5bfac1
f-sajib <f.sajib03@gmail.com>
2022-02-02 12:30:03 +0600

    added redirect page error message on register failed

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -92,6 +92,7 @@ class RegisteredUserController extends Controller
     public function sendOtp($input)
     {
         $otp = mt_rand(100000, 999999);
+
         Cache::put(request()->ip . '_otp', $otp, 120);
         Cache::put(request()->ip . '_input', $input, 120);
         $message = "Your Edventure register otp is " . $otp;
@@ -99,23 +100,25 @@ class RegisteredUserController extends Controller
         $api_key = config("edv.reve_api_key");
         $api_secret = config("edv.reve_secret_key");
         $caller_id = config("edv.reve_caller_id");
+
         $url = "https://smpp.ajuratech.com:7790/sendtext?apikey=" . $api_key . "&secretkey=" . $api_secret . "&callerID=" . $caller_id . "&toUser=" . $input['phone'] . "&messageContent=" . $message;
-        // dd($url);
         try {
             $result = $client->request('GET', $url);
             $result = $result->getBody()->getContents();
             $result = json_decode($result, true);
-            // dd($result);
+//             dd($result);
             if ($result['Status'] == "0") {
                 return view("auth.otp");
             }
         } catch (\GuzzleHttp\Exception\ClientException $e) {
             // catches all ClientExceptions
+//            dd($e);
             return view('auth.register');
-            // dd($e);
+
         } catch (\GuzzleHttp\Exception\ConnectException $e) {
-            // dd($e);
+//             dd($e);
             return view('auth.register');
         }
+        return redirect()->back()->withErrors(['Failed to register! Please try again.']);
     }
 }
