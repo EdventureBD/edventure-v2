dd648302b418c50f1f3eae152d18e6d66f39dbd7
AhmedShibbir <shibbirahmedops@gmail.com>
2022-02-03 18:27:17 +0600

    dashboard backbone build up

--- resources/views/student/pages_new/user/profile.blade.php
+++ resources/views/student/pages_new/user/profile.blade.php
@@ -1,3 +1,34 @@
 <x-landing-layout headerBg="white">
-    
+    <div id="parent-div" class="mt-5 pt-5">
+        <div id="user-info" class="d-flex justify-content-between mx-5 px-5">
+            <div class="border border-primary p-5 row col-gap-5">
+                <div class="mx-2 col-4" id="profile-photo">
+                    <img src="#" alt="phofile photo">
+                </div>
+                <div class="mx-2 col-8 d-flex flex-column justify-content-start" id="profile-course-data">
+                    <div class="" id="">
+                        <h2>Zahid</h2> <i></i>
+                    </div>
+                    <div class="w-100 border border-secondary h-0" id=""></div>
+                    <div class="d-flex justify-content-start gap-5 mt-2" id="">
+                        <div class="d-flex flex-column" id="">
+                            <div class="fw-800" id="">Courses</div>
+                            <div class="fw-800" id="">5</div>
+                        </div>
+                        <div class="d-flex flex-column ml-5" id="">
+                            <div class="fw-800" id="">Completed</div>
+                            <div class="fw-800" id="">3</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="border border-primary p-5 d-flex">
+                <div class="border border-success" style="background: #FA9632;
+                ">Course</div>
+                <div class="border border-success">Model Test</div>
+        </div>
+        <div id="profile-detail">
+
+        </div>
+    </div>
 </x-landing-layout>
