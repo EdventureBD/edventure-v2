7cd3e250b6ace90b63769a7ac4e89def3c3ae3ab
AhmedShibbir <shibbirahmedops@gmail.com>
2022-02-06 15:39:14 +0600

    dashboard profile section modified

--- resources/views/student/pages_new/user/profile.blade.php
+++ resources/views/student/pages_new/user/profile.blade.php
@@ -1,33 +1,60 @@
 <x-landing-layout headerBg="white">
-    <div id="parent-div" class="mt-5 pt-5">
-        <div id="user-info" class="d-flex justify-content-between mx-5 px-5">
-            <div class="border border-primary p-5 row col-gap-5">
-                <div class="mx-2 col-4" id="profile-photo">
-                    <img src="#" alt="phofile photo">
+    <style>
+        #profile-dash-img{
+            width: 165px;
+            display:flex;
+            justify-content: center
+        }
+        @media screen and (max-width:570px){
+            #profile-dash-img{
+                width: 78.5px;
+                margin-left: auto;
+                margin-right: auto;
+                margin-bottom: 15px;
+            }
+        }
+    </style>
+    <div id="parent-div" class="mt-5 pt-5 p-5">
+        <div id="info-part" class="d-md-flex justify-content-between align-items-middle">
+            <div class="d-md-flex justify-content-start mr-auto">
+                <div class="d-md-flex" id="profile-dash-img">
+                    <img src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg" alt="profile-photo" class="img-fluid" style="border-radius: 50%">
                 </div>
-                <div class="mx-2 col-8 d-flex flex-column justify-content-start" id="profile-course-data">
-                    <div class="" id="">
-                        <h2>Zahid</h2> <i></i>
+                <div class="d-flex flex-column justify-content-center align-top ml-3">
+                    <div class="">
+                        <h3>Zahid</h3>
                     </div>
-                    <div class="w-100 border border-secondary h-0" id=""></div>
-                    <div class="d-flex justify-content-start gap-5 mt-2" id="">
-                        <div class="d-flex flex-column" id="">
-                            <div class="fw-800" id="">Courses</div>
-                            <div class="fw-800" id="">5</div>
+                    <div class="w-100 h-0 border border-gray m-0 p-0"></div>
+                    <div class="d-flex pl-3">
+                        <div class="row row-cols-1 fw-800 mr-3">
+                            <div>
+                               Courses 
+                            </div>
+                            <div>
+                                5
+                            </div>
                         </div>
-                        <div class="d-flex flex-column ml-5" id="">
-                            <div class="fw-800" id="">Completed</div>
-                            <div class="fw-800" id="">3</div>
+                        <div class="row row-cols-1 fw-800 ml-3">
+                            <div>
+                                Completed
+                            </div>
+                            <div>
+                                3
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="border border-primary p-5 d-flex">
-                <div class="border border-success" style="background: #FA9632;
-                ">Course</div>
-                <div class="border border-success">Model Test</div>
+            <div class="d-flex max-h-10">
+                <div class="px-5 py-2 border my-auto fw-800">
+                    Course
+                </div>
+                <div class="px-5 py-2 border my-auto fw-800 text-white" style="background: #FA9632;">
+                    Model Test
+                </div>
+            </div>
         </div>
-        <div id="profile-detail">
+        <div id="info-detail">
 
         </div>
     </div>
