cc58fe06cc7dc6c77dee5212e2ad2c3ae17f4b12
f-sajib <f.sajib03@gmail.com>
2022-02-16 16:06:02 +0600

    added global loading

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        Sarfraznawaz2005\Loading\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
