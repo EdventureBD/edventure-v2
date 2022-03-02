d45347d23b463eaa0330dcf43506500991215270
AhmedShibbir <shibbirahmedops@gmail.com>
2022-03-02 14:04:38 +0600

    landing page's image changed

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -28,12 +28,12 @@
     </section>
     <div class="our-package-section text-center py-5">
         <div class="container">
-            <h3 class="text-purple text-md font-roboto">কি পাবে আমাদের প্ল্যাটফর্মে</h3>
-            <p class="fw-600 text-xxsm max-w-38 w-100 mx-auto text-purple-half">- স্বয়ংক্রিয় ড্যাশবোর্ডের সাহায্যে নিজের Strength এবং Weakness মূল্যায়ন করার উপায়
+            <h3 class="text-dark text-md font-roboto">কি পাবে আমাদের প্ল্যাটফর্মে</h3>
+            <p class="fw-600 text-xxsm max-w-38 w-100 mx-auto text-dark">- স্বয়ংক্রিয় ড্যাশবোর্ডের সাহায্যে নিজের Strength এবং Weakness মূল্যায়ন করার উপায়
                 - ডিজিটাল রিপোর্টের মাধ্যমে নিমিষেই জেনে যাবে এক্সামের সকল খুঁটিনাটি বিষয়
                 - দেশসেরা অভিজ্ঞ শিক্ষকের সল্‌ভ ক্লাস ও সাজেশন
                 </p>
-            <div class="single-package bg-art py-5 px-3 mt-6">
+            <div class="single-package py-5 px-3 mt-6">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="ml-4">
@@ -44,11 +44,11 @@
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <img src="/img/medical_camp/medicamp12.png" class=" img-fluid" alt="Roadmap Image">
+                        <img src="/img/landing/roadmapCardBanner.png" class=" img-fluid" alt="Roadmap Image">
                     </div>
                 </div>
             </div>
-            <div class="single-package bg-art py-5 px-3 mt-6">
+            <div class="single-package py-5 px-3 mt-6">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="ml-4">
@@ -59,11 +59,11 @@
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <img src="/img/medical_camp/medicamp22.png" class="img-fluid" alt="Dashboard Photo">
+                        <img src="/img/landing/phoneCartBanner.png" class="img-fluid" alt="Dashboard Photo">
                     </div>
                 </div>
             </div>
-            <div class="single-package bg-art py-5 px-3 mt-6">
+            <div class="single-package py-5 px-3 mt-6">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="ml-4">
