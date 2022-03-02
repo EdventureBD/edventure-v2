74d6c5c3081047363742c453fb69b13a6496b626
AhmedShibbir <shibbirahmedops@gmail.com>
2022-03-02 14:17:32 +0600

    route changed for landing page

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -8,7 +8,7 @@
                         <h4 class="text-white  text-sm font-roboto">Hey, {{Auth::user()->name}} !</h4><br>
                         <h4 class="text-white  text-sm font-roboto">Welcome to the Edventure.</h4>
                         <h2 class="text-sm fw-800 font-roboto mb-2 text-white">প্রস্তুতি হোক <br> নিজের মতো<h2><br>
-                        <a href="{{route('profile.modelTest')}}" class="font-roboto text-xsm btn btn-register btn-orange-customed text-white px-4" style="border: 1px solid #FA9632">Go to Dashboard</a>
+                        <a href="{{route('model.exam')}}" class="font-roboto text-xsm btn btn-register btn-orange-customed text-white px-4" style="border: 1px solid #FA9632">Go For Exam</a>
                         @else
                         <div id="landing_greeting_and_register_button">
                             <div>
