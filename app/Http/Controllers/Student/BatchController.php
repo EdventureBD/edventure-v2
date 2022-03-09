019cc67e9db141f7bf8015110c5530e8a3ba803b
max-fletcher <amar.naam.machine@gmail.com>
2022-03-09 12:33:06 +0600

    Added logic to show 1,2 & 3 star images. Now only disabled image is needed.

--- app/Http/Controllers/Student/BatchController.php
+++ app/Http/Controllers/Student/BatchController.php
@@ -40,12 +40,17 @@ class BatchController extends Controller
 
         $island_images = [];
         foreach($batchTopics as $batchTopic){
-            $island_images[] = $batchTopic->courseTopic->island_image;
+            // nodes can be lectures or courses. Counting how many out of total are completed.
+            $number_of_nodes = 0;
+            $number_of_completed_nodes = 0;
+            // $island_images[] = $batchTopic->courseTopic->island_image;
             foreach($batchTopic->courseTopic->exams as $exam){
+                $number_of_nodes++;
                 $scored_marks = 0;
                 $details_results = DetailsResult::where('exam_id', $exam->id)->where('exam_type', $exam->exam_type)->where('student_id', auth()->user()->id)->get();
 
                 if($exam->exam_type === "Aptitude Test" ){
+                    $number_of_completed_nodes++;
                     if(count($details_results)){
                         $exam->has_been_attempted = true;
                     }
@@ -59,6 +64,9 @@ class BatchController extends Controller
                 }
                 
                 if($scored_marks >= $exam->threshold_marks){
+                    if($exam->exam_type !== "Aptitude Test" ){
+                        $number_of_completed_nodes++;
+                    }
                     $exam->test_passed = true;
                 }
                 else{
@@ -68,8 +76,10 @@ class BatchController extends Controller
                 $lectures_in_this_exam = count($exam->course_lectures);
                 $completed_lecture_count = 0;
                 foreach($exam->course_lectures as $lecture){
+                    $number_of_nodes++;
                     $completed = CompletedLectures::where('student_id', auth()->user()->id)->where('lecture_id', $lecture->id)->count();
                     if($completed){
+                        $number_of_completed_nodes++;
                         $lecture->completed = true;
                         $completed_lecture_count++;
                     }
@@ -80,7 +90,27 @@ class BatchController extends Controller
                 $exam->completed_lecture_count = $completed_lecture_count;
                 $exam->scored_marks = $scored_marks;
             }
+
+            // calculate completion percentage using total nodes and completed nodes
+            if($number_of_nodes == 0){
+                $batchTopic->percentage_completion = 0;
+            }
+            else{
+                $batchTopic->percentage_completion = ($number_of_completed_nodes/$number_of_nodes)*100;
+            }
+
+            if($batchTopic->percentage_completion > 66.66){
+                $island_images[] = $batchTopic->courseTopic->three_star_island_image;
+            }
+            elseif($batchTopic->percentage_completion > 33.33){
+                $island_images[] = $batchTopic->courseTopic->two_star_island_image;
+            }
+            elseif($batchTopic->percentage_completion >= 0){
+                $island_images[] = $batchTopic->courseTopic->one_star_island_image;
+            }
         }
+
+        // dd($batchTopics);
 
         $accessedDays = BatchStudentEnrollment::where('student_id', auth()->user()->id)
             ->where('batch_id', $batch->id)
