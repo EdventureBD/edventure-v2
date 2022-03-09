e2df2a30df3c34b1842cb80215149cefb96d75cf
AhmedShibbir <shibbirahmedops@gmail.com>
2022-03-09 11:49:53 +0600

    multistep signup page's backbone setup

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -22,7 +22,8 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('auth.register');
+        // return view('auth.register');
+        return view('auth.new.register');
     }
 
     /**
---
+++ resources/views/auth/new/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Edventure | Signup</title>
+        {{-- cdn links for css starts  --}}
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+        {{-- cdn links for css starts --}}
+
+        {{-- custom css link starts  --}}
+        <link rel="stylesheet" href="/css/new-multistep-signup.css">
+        {{-- custom css link ends  --}}
+    </head>
+    <body>
+        @yield('content')
+
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        {{-- custom js link  --}}
+        <script src="/js/new-multistep-signup.js"></script>
+    </body>
+</html>
