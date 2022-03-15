0b3f393825ff205b9f353df316d1c78560b4e825
f-sajib <f.sajib03@gmail.com>
2022-03-15 11:10:21 +0600

    modified district enum

--- app/Enum/District.php
+++ app/Enum/District.php
@@ -5,14 +5,14 @@ namespace App\Enum;
 interface District {
 
     const List = [
-        '1' => 'Dhaka',
-        '2' => 'Chittagong',
-        '3' => 'Rajshahi',
-        '4' => 'Khulna',
-        '5' => 'Barisal',
-        '6' => 'Sylhet',
-        '7' => 'Mymensingh',
-        '8' => 'Rangpur',
+        'dhaka' => 'Dhaka',
+        'chittagong' => 'Chittagong',
+        'rajshahi' => 'Rajshahi',
+        'khulna' => 'Khulna',
+        'barisal' => 'Barisal',
+        'sylhet' => 'Sylhet',
+        'mymensingh' => 'Mymensingh',
+        'rangpur' => 'Rangpur',
 
     ];
 }
