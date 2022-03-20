fcbe2550b1814be40c456fc570cade1e784d3458
f-sajib <f.sajib03@gmail.com>
2022-03-20 10:56:54 +0600

    added social group data insertion from admin side

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ModelMcqTagAnalysisController;
 use App\Http\Controllers\SinglePaymentController;
+use App\Http\Controllers\SocialGroupController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ExamCategoryController;
@@ -295,6 +296,11 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'is_admin']], functi
 
     /**************************************** Model Exam ****************************************/
 
+
+    Route::get('/social-group',[SocialGroupController::class,'index'])->name('social.group.index');
+    Route::post('/social-group',[SocialGroupController::class,'store'])->name('social.group.store');
+    Route::delete('/social-group/{id}',[SocialGroupController::class,'destroy'])->name('social.group.delete');
+    Route::put('/social-group/{id}',[SocialGroupController::class,'update'])->name('social.group.update');
 
 });
 
