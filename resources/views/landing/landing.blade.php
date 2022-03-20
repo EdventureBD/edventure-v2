88a601026a1d8bb19060e589adcf5be0df174417
AhmedShibbir <shibbirahmedops@gmail.com>
2022-03-20 18:27:44 +0600

    landing page's new design's part's backbone buildup

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -1,4 +1,5 @@
 <x-landing-layout>
+    
     {{-- new landing banner part starts --}}
     <section class="position-fixed w-100">
         <div class="row pt-5 pb-0 mx-0 px-0 my-0 ">
@@ -31,12 +32,12 @@
     </section>
     {{-- new landing banner part ends --}}
     <div class="our-package-section text-center py-5 position-relative bg-white">
-        <div class="container">
+        <div class="">
             <h3 class="text-dark text-md font-roboto mb-5">কী পাবে আমাদের প্ল্যাটফর্মে</h3>
             <p class="fw-600 text-xxsm max-w-100 w-100 mx-auto text-dark">- স্বয়ংক্রিয় ড্যাশবোর্ডের সাহায্যে নিজের Strength এবং Weakness মূল্যায়ন করার উপায়</p>
             <p class="fw-600 text-xxsm max-w-100 w-100 mx-auto text-dark">- ডিজিটাল রিপোর্টের মাধ্যমে নিমিষেই জেনে যাবে এক্সামের সকল খুঁটিনাটি বিষয়</p>
             <p class="fw-600 text-xxsm max-w-100 w-100 mx-auto text-dark">- দেশসেরা অভিজ্ঞ শিক্ষকের সল্‌ভ ক্লাস ও সাজেশন</p>
-            <div class="single-package py-5 px-3 mt-6">
+            {{-- <div class="single-package py-5 px-3 mt-6">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="ml-4">
@@ -78,6 +79,54 @@
                     </div>
                     <div class="col-md-6">
                         <img src="/img/landing/newLanding/discussionWithTeacher.webp" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div> --}}
+            <div class="d-flex align-items-center flex-sm-column flex-md-row my-0 py-0" id="syllabus-mapping">
+                <div class="d-flex justify-content-sm-center justify-content-md-start" style="width: 65%">
+                    <div class="w-100 d-flex justify-content-start">
+                        <img src="/img/landing/newLanding/roadMap.webp" alt="roadmap" class="img-fluid">
+                    </div>
+                </div>
+                <div class="d-flex flex-column" style="width: 35%">
+                    <div class="w-25 ml-auto mr-5">
+                        <img src="/img/medical_camp/medicamp11.png" alt="" class="img-fluid">
+                    </div>
+                    <div class="mx-5">
+                        <h4 class="text-xmd fw-800 text-right mt-3">সিলেবাস ম্যাপিং</h4>
+                        <p class="text-xsm fw-400 text-gray text-right">পরীক্ষার প্রস্তুতিতে বসে সিলেবাসের মহাসমুদ্রে যাতে হারিয়ে না যাও- সেজন্য আমরা পুরো সিলেবাসকে ম্যাপ করে দিচ্ছি</p>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex align-items-center flex-sm-column flex-md-row-reverse my-0 py-0" id="weakness-detection">
+                <div class="d-flex justify-content-end" style="width: 65%">
+                    <div class="w-100 d-flex justify-content-end">
+                        <img src="/img/landing/newLanding/mobileDashboard.webp" alt="roadmap" class="img-fluid">
+                    </div>
+                </div>
+                <div class="d-flex flex-column" style="width: 35%">
+                    <div class="w-25 mx-5">
+                        <img src="/img/medical_camp/medicamp21.png" alt="" class="img-fluid">
+                    </div>
+                    <div class="mx-5">
+                        <h4 class="text-xmd fw-800 text-left mt-3">টপিকভিত্তিক উইকনেস ডিটেকশন</h4>
+                        <p class="text-xsm fw-400 text-gray text-left">বাংলাদেশের একমাত্র প্ল্যাটফর্ম হিসেবে Edventure এ পরীক্ষা দিয়েই জানতে পারবে ঠিক কোন চ্যাপ্টারের কোন টপিকে তুমি দুর্বল</p>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex align-items-center flex-sm-column flex-md-row my-0 py-0" id="personal-session">
+                <div class="d-flex justify-content-start" style="width: 65%">
+                    <div class="w-100 d-flex justify-content-start">
+                        <img src="/img/landing/newLanding/socialGroup.webp" alt="discussion banner image" class="img-fluid">
+                    </div>
+                </div>
+                <div class="d-flex flex-column" style="width: 35%">
+                    <div class="w-25 ml-auto mr-5">
+                        <img src="/img/medical_camp/medicamp31.png" alt="" class="img-fluid">
+                    </div>
+                    <div class="mx-5">
+                        <h4 class="text-xmd fw-800 text-right mt-3">টিচারদের সাথে পারসোনাল সেশন</h4>
+                        <p class="text-xsm fw-400 text-gray text-right">দেশসেরা শিক্ষকদের সাথে পারসোনাল সেশন বুক করে তোমার সমস্যা শেয়ার করতে পারো, সমাধান দিবে তারাই!</p>
                     </div>
                 </div>
             </div>
