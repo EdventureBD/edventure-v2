864ec9417e5e88c44c3440dbe0f45fa1b4c87522
AhmedShibbir <shibbirahmedops@gmail.com>
2022-03-31 16:28:36 +0600

    card in select category page's design updated

--- resources/views/student/pages_new/model-exam/category.blade.php
+++ resources/views/student/pages_new/model-exam/category.blade.php
@@ -1,67 +1,42 @@
 <x-landing-layout headerBg="white">
-    <style>
-        #select-category {
-            margin-top: 10px;
-            margin-bottom: 33px;
-            font-weight: 500;
-            font-size: 22px;
-        }
-        @media screen and (max-width:768px) and (min-width: 577px) {
-            .category-cards-parent {
-                margin-left: 25px !important;
-            }
-        }
-        @media screen and (max-width:576px) {
-            .category-cards-parent {
-                margin-left: 33px !important;
-            }
-        }
-    </style>
     <link rel="stylesheet" href="/css/model-exam-index.css">
+
     <div class="page-section">
         <div class="container">
             @include('partials.alert')
 
-            @if(count($exam_categories) > 0)
+            @if (count($exam_categories) > 0)
                 <div class="py-4">
-                    <div class="text-center bradius-10 py-2 w-100 text-sm " style="font-weight: 900; color: #76777A"> Exams Category</div>
-                    @if(!request()->has('c'))
+                    <div class="text-center bradius-10 py-2 w-100 text-sm " style="font-weight: 900; color: #76777A">
+                        Exams Category</div>
+                    @if (!request()->has('c'))
                         <div class="text-center" id="select-category"> Select a Category</div>
                     @endif
-                    <div class="row row-cols-lg-4 row-cols-md-2 row-cols-sm-1 category-cards-parent" style="row-gap: 35px">
-                        @foreach($exam_categories as $category)
-                            {{-- <div class="col-md-4">
-                                <div class="col-md-3 mb-4" style="max-width: 100%;padding-right: 0 !important;">
-                                    <div class="single-exam text-center mx-auto p-4 mb-md-0">
-                                        <h5 style="line-height: 1.5em; height: 3em; width: 100%; overflow: hidden;" class="text-center mt-2">{{ $category->name }} </h5>
-                                        <div>
-                                            @if($category->image)
-                                            <img height="100" width="100" src="{{Storage::url('categoryImage/'.$category->image)}}" alt="">
-                                            @endif
-                                        </div>
-                                        <div class="text-center d-block custom-shadow">
-                                            <a
-                                                href="{{route('model.exam',['c' => $category->uuid])}}"
-                                                class="btn btn-outline text-purple mt-2">
-                                                See Details
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> --}}
+                    <div class="row row-cols-lg-4 row-cols-md-2 row-cols-sm-1 category-cards-parent"
+                        style="row-gap: 35px">
+                        @foreach ($exam_categories as $category)
                             <div class="">
-                                <div class="card text-center" style="border-radius: 26px;padding: 14px 14px !important;width: 248px;
-                                height: 301px;">
-                                    <div class="card-header fw-800 text-capitalize">
-                                        {{ $category->name }} 
+                                <div class="card text-center"
+                                    style="border-radius: 26px;padding: 14px 14px !important;width: 248px;height: 301px;">
+                                    <div class="card-header fw-800"
+                                        style="color: #6400C8;font-size: 22px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;"
+                                        data-toggle="tooltip" data-placement="right" title="{{ $category->name }}">
+                                        {{ $category->name }}
                                     </div>
                                     <div class="card-body">
-                                         @if($category->image)
-                                            <img class="img-fluid" src="{{Storage::url('categoryImage/'.$category->image)}}" alt="">
+                                        @if ($category->image)
+                                            <img class="img-fluid" height="96" width="112"
+                                                src="{{ Storage::url('categoryImage/' . $category->image) }}"
+                                                alt="Exam category image">
+                                        @else
+                                            <img class="img-fluid" height="96" width="112"
+                                                src="/img/category_details/default-image.png" alt="Exam category image">
                                         @endif
+
                                     </div>
                                     <div class="card-footer text-muted">
-                                        <a href="{{route('model.exam',['c' => $category->uuid])}}" class="btn btn-orange-customed">See Detail</a>
+                                        <a href="{{ route('model.exam', ['c' => $category->uuid]) }}"
+                                            class="btn text-white fw-700 btn-detail">See Detail</a>
                                     </div>
                                 </div>
                             </div>
@@ -69,7 +44,8 @@
                     </div>
                 </div>
                 <div class="py-5 py-md-1 text-center d-flex justify-content-center">
-                    <p class="text-center">{{ $exam_categories->withQueryString()->links('vendor.pagination.custom') }}</p>
+                    <p class="text-center">
+                        {{ $exam_categories->withQueryString()->links('vendor.pagination.custom') }}</p>
                 </div>
             @else
                 <div class="py-4">
@@ -82,22 +58,27 @@
                 </div>
             @endif
 
-            </div>
+        </div>
     </div>
-    </div>
 
-    {{--    /************************* Sweet Alert ******************************/--}}
+    {{-- /************************* Sweet Alert ******************************/ --}}
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.9/sweetalert2.min.css" integrity="sha512-cyIcYOviYhF0bHIhzXWJQ/7xnaBuIIOecYoPZBgJHQKFPo+TOBA+BY1EnTpmM8yKDU4ZdI3UGccNGCEUdfbBqw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.9/sweetalert2.all.min.js" integrity="sha512-IZ95TbsPTDl3eT5GwqTJH/14xZ2feLEGJRbII6bRKtE/HC6x3N4cHye7yyikadgAsuiddCY2+6gMntpVHL1gHw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.9/sweetalert2.min.css"
+        integrity="sha512-cyIcYOviYhF0bHIhzXWJQ/7xnaBuIIOecYoPZBgJHQKFPo+TOBA+BY1EnTpmM8yKDU4ZdI3UGccNGCEUdfbBqw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.9/sweetalert2.all.min.js"
+        integrity="sha512-IZ95TbsPTDl3eT5GwqTJH/14xZ2feLEGJRbII6bRKtE/HC6x3N4cHye7yyikadgAsuiddCY2+6gMntpVHL1gHw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    {{--    /*******************************************************/--}}
+    {{-- /*******************************************************/ --}}
 
     <script>
         var loginAlert = document.getElementsByClassName('logInAlert');
 
-        for(var i = 0; i < loginAlert.length; i++) {
+        for (var i = 0; i < loginAlert.length; i++) {
             (function(index) {
                 loginAlert[index].addEventListener("click", function() {
                     Swal.fire({
