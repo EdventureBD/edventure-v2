b581cd03ac0695c2ea50cb073ded914f71130d70
f-sajib <f.sajib03@gmail.com>
2022-04-03 09:40:17 +0600

    added eggerlooader on user list query

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function index()
     {
         // WITHOUT LIVEWIRE
-        $users = User::query();
+        $users = User::query()->with('studentDetails');
         $users = $this->getUser($users);
         return view('admin.pages.user.index', compact('users'));
 
@@ -42,7 +42,7 @@ class UserController extends Controller
 
     public function allAdmin()
     {
-        $users = User::where('user_type', 1);
+        $users = User::where('user_type', 1)->with('studentDetails');
         $users = $this->getUser($users);
         return view('admin.pages.user.index', compact('users'));
 
@@ -53,7 +53,7 @@ class UserController extends Controller
 
     public function allTeacher()
     {
-        $users = User::where('user_type', 2);
+        $users = User::where('user_type', 2)->with('studentDetails');
         $users = $this->getUser($users);;
         return view('admin.pages.user.index', compact('users'));
 
