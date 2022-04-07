e21f64a2ef9c17b0466a2f7b95a77ebedd4d34fe
AhmedShibbir <shibbirahmedops@gmail.com>
2022-04-07 10:35:27 +0600

    model exam index page's card's new design updated

--- resources/views/student/pages_new/model-exam/index.blade.php
+++ resources/views/student/pages_new/model-exam/index.blade.php
@@ -2,6 +2,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.9/sweetalert2.min.css" integrity="sha512-cyIcYOviYhF0bHIhzXWJQ/7xnaBuIIOecYoPZBgJHQKFPo+TOBA+BY1EnTpmM8yKDU4ZdI3UGccNGCEUdfbBqw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/css/model-exam-index.css">
+    <link rel="stylesheet" href="/css/tooltip.css">
     <style>
         .bold-header {
             font-weight: bolder;
@@ -11,10 +12,10 @@
 <div class="page-section">
     <div class="container">
         @include('partials.alert')
-        <h1 class="text-center mt-6 bold-header">{{$category->name}}</h1>
+        <h3 class="text-center mt-6 bold-header">{{$category->name}}</h3>
         @if(count($exam_topics) > 0 )
-            <div class="mt-1">
-                <div class=" text-center bradius-10 py-2 w-100 text-gray text-sm fw-700">Topics</div>
+            <div class="my-md-3 my-sm-2">
+                <div class=" text-center bradius-10 py-2 w-100 text-gray text-xsm fw-700">Topics</div>
             </div>
             <div class="text-center @if($exam_topics->count()>=7) course-category-js @endif ">
                 @foreach($exam_topics as $topic)
@@ -34,7 +35,7 @@
         @endif
 
             @if(count($exams) > 0 )
-                <div class="row justify-content-center py-3 card-group-row mb-lg-8pt">
+                <div class="row row-cols-lg-4 justify-content-center py-3 card-group-row mb-lg-8pt" style="row-gap: 25px">
 
                     @foreach ($exams as $exam)
                         @php($label = 'Take Exam')
@@ -70,27 +71,103 @@
                                 @endif
                             @endforeach
                         @endif
-                        <div class="col-md-3 mb-4" style="max-width: 100%;padding-right: 0 !important;">
+                        {{-- <div class="col-md-3 mb-4" style="max-width: 100%;padding-right: 0 !important;">
                             <div class="{{$d_none}} ribbon ribbon-top-left"><span>done</span></div>
-                            <div style="background-position: center center !important;background-size:cover !important;
-                                background: url({{$exam->image ? Storage::url('examImage/'.$exam->image) : ''}})"
+                            <div style=""
                                  class="single-exam text-center mx-auto p-4 mb-md-0">
-                                <h5 style="line-height: 1.5em; height: 3em; width: 100%; overflow: hidden;" class="text-center mt-2">{{ $exam->title }} </h5>
+                            
+                                <div class="card-header text-center mt-2 fw-800"
+                                    style="color: #6400C8;font-size: 16px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;"
+                                    data-toggle="tooltip" 
+                                    data-placement="top" 
+                                    title="{{ $exam->title }}">
+                                    {{ $exam->title }}
+                                </div>
                                 @if(!is_null($exam->exam_price) && $exam->exam_price != 0)
-                                    <p class=" text-center text-md mt-2 fw-600 text-price">{{(int)($exam->exam_price)}}৳</p>
+                                    <p class=" text-center text-sm mt-2 fw-600 text-price">{{(int)($exam->exam_price)}}৳</p>
                                 @else
-                                    <p class=" text-center text-md mt-2 fw-600 text-price">Free</p>
+                                    <p class=" text-center text-sm mt-2 fw-600 text-price">Free</p>
                                 @endif
-                                <div style="margin-top: 40px" class="text-center d-block">
+                                <div class="card-body">
+                                    @if ($exam->image)
+                                        <img class="img-fluid" height="96" width="112"
+                                            src="{{$exam->image ? Storage::url('examImage/'.$exam->image) : ''}}"
+                                            alt="Exam image">
+                                    @else
+                                        <img class="img-fluid" height="96" width="112"
+                                            src="/img/category_details/default-image.png" alt="Exam image">
+                                    @endif
+        
+                                </div>
+
+                                <div class="card-footer text-muted">
                                     <a
                                         href="{{!auth()->check() ? 'javascript:void(0);' : $href}}"
-                                        class="{{auth()->check() && auth()->user()->is_admin == 1 ? 'disabled' : ''}}{{!auth()->check() ? 'logInAlert' : ''}} btn btn-outline text-purple mt-2">
+                                        class="{{auth()->check() && auth()->user()->is_admin == 1 ? 'disabled' : ''}}{{!auth()->check() ? 'logInAlert' : ''}} btn text-white fw-700 btn-detail">
                                         {{$label}}
+                                    </a>
+                                </div>
+                            </div>
+                        </div> --}}
+                        <div class="">
+                            <div class="card text-center"
+                                style="border-radius: 26px;padding: 14px 14px !important;width: 248px;height: 301px;">
+                                <div class="{{$d_none}} ribbon ribbon-top-left"><span>done</span></div>
+                                <div class="card-header fw-800"
+                                    style="color: #6400C8;font-size: 16px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;"
+                                    data-toggle="tooltip" 
+                                    data-placement="top" 
+                                    title="{{ $exam->title }}">
+                                    {{ $exam->title }}
+                                </div>
+                                <div class="card-body">
+                                    @if ($exam->image)
+                                        <img class="img-fluid" height="96" width="112"
+                                            src="{{$exam->image ? Storage::url('examImage/'.$exam->image) : ''}}"
+                                            alt="Exam image">
+                                    @else
+                                        <img class="img-fluid" height="96" width="112"
+                                            src="/img/category_details/default-image.png" alt="Exam image">
+                                    @endif
+
+                                </div>
+                                <div class="card-footer text-muted">
+                                    <a href="{{!auth()->check() ? 'javascript:void(0);' : $href}}"
+                                                class="{{auth()->check() && auth()->user()->is_admin == 1 ? 'disabled' : ''}}{{!auth()->check() ? 'logInAlert' : ''}} btn text-white fw-700 btn-detail">
+                                                {{$label}}
                                     </a>
                                 </div>
                             </div>
                         </div>
                     @endforeach
+                    {{--######################## new design starts++++++++++++++++++++++++++++++++ --}}
+                    {{-- <div class="card text-center"
+                        style="border-radius: 26px;padding: 14px 14px !important;width: 248px;height: 301px;">
+                        <div class="{{$d_none}} ribbon ribbon-top-left"><span>done</span></div>
+                        <div class="card-header fw-800"
+                            style="color: #6400C8;font-size: 16px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;"
+                            data-toggle="tooltip" 
+                            data-placement="top" 
+                            title="Exam Name">
+                            Exam Name
+                        </div>
+                        <div class="card-body">
+                            @if ($exam)
+                                <img class="img-fluid" height="96" width="112"
+                                    src="{{$exam->image ? Storage::url('examImage/'.$exam->image) : ''}}"
+                                    alt="Exam image">
+                            @else
+                                <img class="img-fluid" height="96" width="112"
+                                    src="/img/category_details/default-image.png" alt="Exam image">
+                            @endif
+
+                        </div>
+                        <div class="card-footer text-muted">
+                            <a href="#"
+                                class="btn text-white fw-700 btn-detail">Take Exam</a>
+                        </div>
+                    </div> --}}
+                    {{--################################# new design ends +++++++++++++++++++++++++++ --}}
                 </div>
                 <div class="py-5 py-md-1 text-center d-flex justify-content-center">
                     <p class="text-center">{{ $exams->withQueryString()->links('vendor.pagination.custom') }}</p>
@@ -124,4 +201,9 @@
         }
     </script>
 </x-landing-layout>
+<script>
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
 
