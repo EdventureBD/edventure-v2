b32ced6d6669478324016fa55b5d4ad46b281730
max-fletcher <amar.naam.machine@gmail.com>
2022-04-07 14:24:41 +0600

    Changed Exam attempted page bg-color and text color. Commiting before changing roadmap data.

--- resources/views/student/pages_new/batch/exam/batch_exam_not_checked.blade.php
+++ resources/views/student/pages_new/batch/exam/batch_exam_not_checked.blade.php
@@ -13,10 +13,10 @@
                   <p class="hero__lead measure-hero-lead text-white-50">Exam : {{ $exam->title }}</p>
             </div>
          </div>
-         <div class="bg-light-gray">
+         <div class="" style="background: #75E9AA !important;">
             <div class="container">
                   <div class="py-5" style="display: flex; flex-direction: column;">
-                     <h1 class="display-5 text-sm text-dark max-w-38 mx-auto fw-600" style="text-align: center">Your submitted answer is being reviewed. Please wait</h1>
+                     <h1 class="display-5 text-sm text-purple max-w-38 mx-auto fw-600" style="text-align: center">You have successfully attempted this exam. However, it is still yet to be checked. Visit later to see your results.</h1>
                      <div class="d-flex mx-auto">
                         <a href="{{ route('batch-lecture', ['batch' => $batch->slug]) }}" class="btn text-xxsm text-white bg-purple fw-800 px-3 py-2 w-20 mb-3 mt-3 mx-1"> Go Back to Journey <i class="fas fa-arrow-up ml-2"></i></a>
                         @if(!empty($next_link)) <a href="{{ $next_link }}" class="btn text-xxsm text-white bg-purple fw-800 px-3 py-2 w-20 mb-3 mt-3 mx-1">Next <i class="fas fa-angle-double-right ml-1"></i></a> @endif
