283bc497dc41258dd401cdb119d1ea82ac5f3516
max-fletcher <amar.naam.machine@gmail.com>
2022-04-09 11:04:30 +0600

    Changed Intermediary Level name to Programs on frontend.

--- resources/views/livewire/bundle/edit.blade.php
+++ resources/views/livewire/bundle/edit.blade.php
@@ -115,10 +115,10 @@
                                     </div>
                                     <div class="col-md-4">
                                         <div class="form-group">
-                                            <label class="col-form-label" for="bundleName">Intermediary Level <span
+                                            <label class="col-form-label" for="bundleName">Program <span
                                                     class="must-filled">*</span></label>
                                             <select class="form-control" wire:model="intermediaryLevelId">
-                                                <option value="" selected>Select Intermediary Level</option>
+                                                <option value="" selected>Select Program</option>
                                                 @foreach ($intermediary_levels as $intermediary)
                                                     <option value="{{ $intermediary->id }}">{{ $intermediary->title }}</option>
                                                 @endforeach
--- resources/views/livewire/intermediary-level/edit.blade.php
+++ resources/views/livewire/intermediary-level/edit.blade.php
@@ -9,7 +9,7 @@
                         <!-- general form elements disabled -->
                         <div class="card card-warning">
                             <div class="card-header">
-                                <h3 class="card-title">Update Intermediary Level</h3>
+                                <h3 class="card-title">Update Program</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body">
