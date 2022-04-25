35ff3b6b4528c12c906ff9bda74ec5b236f90b17
AhmedShibbir <shibbirahmedops@gmail.com>
2022-04-25 15:03:32 +0600

    course category part updated

--- resources/views/student/pages_new/course/preview_guest.blade.php
+++ resources/views/student/pages_new/course/preview_guest.blade.php
@@ -1,6 +1,6 @@
 {{--previous student/pages/course/course_preview.blade.php--}}
 
-<x-landing-layout headerBg="white">
+{{-- <x-landing-layout headerBg="white">
     <div class="page-section course-info bg-gradient-purple py-5 px-3 px-md-0">
         <div class="container">
             <div class="row align-items-center">
@@ -103,4 +103,331 @@
         modalIframe.src = modalIframe.src;
     }
     </script>
+</x-landing-layout> --}}
+
+{{-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++new design starts ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ --}}
+
+<x-landing-layout headerBg="white">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.9/sweetalert2.min.css" integrity="sha512-cyIcYOviYhF0bHIhzXWJQ/7xnaBuIIOecYoPZBgJHQKFPo+TOBA+BY1EnTpmM8yKDU4ZdI3UGccNGCEUdfbBqw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- custom css link  --}}
+    <link rel="stylesheet" href="/css/course-detail.css">
+    {{-- custom css link ends  --}}
+<div class="page-section">
+    <div class="container">
+        <div class="d-flex mb-3" id="parent-div">
+            <div class="w-100  mt-5">
+                <h1 class="bold-header">
+                    {{-- {{$category->name}} --}}
+                </h1>
+
+                <div class="includes">
+                    <div class="row">
+                        <div class="col-md-6 d-flex my-2">
+                            <div class="col-4 p-0 my-auto">
+                                <img class="img-fluid" src="/img/category_details/participant.png" alt="participant's photo"/>
+                            </div>
+                            <div class="col-8 d-flex flex-column p-0 my-auto">
+                                <div class="text-nowrap detail-parts-font">কোর্সটি করছেন </div>
+                                <div class="text-nowrap detail-parts-font">
+                                    {{-- @if(!is_null($category->price) && $category->price != 0)
+                                        @php($number = $category->payment_of_categories_count <= 83 ? 83 : $category->payment_of_categories_count)
+                                        {{\App\Enum\Converter::en2bn($number)}} জন
+                                    @else
+                                        @php($number = $category->total_participation_count <= 83 ? 83 : $category->total_participation_count)
+                                        {{\App\Enum\Converter::en2bn($number)}} জন
+                                    @endif --}}
+                                </div>
+                            </div>
+                        </div>
+
+                        @if(true)
+                            <div class="col-md-6 d-flex my-2">
+                                <div class="col-4 p-0 my-auto">
+                                    <img class="img-fluid" src="/img/category_details/timer.png" alt="timer's photo">
+                                </div>
+                                <div class="col-8 d-flex flex-column p-0 my-auto">
+                                    <div class="text-nowrap detail-parts-font">সময় লাগবে </div>
+                                    <div class="text-nowrap detail-parts-font">
+                                        {{-- {{\App\Enum\Converter::en2bn($category->time_allotted)}} --}}1 ঘন্টা
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+
+                        @if(true)
+                            <div class="col-md-6 d-flex my-2">
+                                <div class="col-4 p-0 my-auto">
+                                    <img class="img-fluid" src="/img/category_details/completeSolution.png" alt="completeSolution">
+                                </div>
+                                <div class="col-8 d-flex flex-column p-0 my-auto">
+                                    <div class="text-nowrap detail-parts-font">ভিডিও লেকচার </div>
+                                    <div class="text-nowrap detail-parts-font">
+                                        250 <span style="font-size: 20px;font-weight: 600;">+</span>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+
+                        @if(true)
+                            <div class="col-md-6 d-flex my-2">
+                                <div class="col-4 p-0 my-auto">
+                                    <img class="img-fluid" src="/img/category_details/paperFinal.png" alt="paperFinal">
+                                </div>
+                                <div class="col-8 d-flex flex-column p-0 my-auto">
+                                    <div class="text-nowrap detail-parts-font">নোটস</div>
+                                    <div class="text-nowrap detail-parts-font">
+                                        10 <span style="font-size: 20px;font-weight: 600;">+</span>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+
+
+                        @if(true)
+                            <div class="col-md-6 d-flex my-2">
+                                <div class="col-4 p-0 my-auto">
+                                    <img class="img-fluid" src="/img/category_details/subjectFinal.png" alt="subjectFinal">
+                                </div>
+                                <div class="col-8 d-flex flex-column p-0 my-auto">
+                                    <div class="text-nowrap detail-parts-font">কুইজ</div>
+                                    <div class="text-nowrap detail-parts-font">
+                                        50 টি
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+
+
+                        @if(true)
+                            <div class="col-md-6 d-flex my-2">
+                                <div class="col-4 p-0 my-auto">
+                                    <img class="img-fluid" src="/img/category_details/modelTest.png" alt="FinalModelTest">
+                                </div>
+                                <div class="col-8 d-flex flex-column p-0 my-auto">
+                                    <div class="text-nowrap detail-parts-font">মাইন্ড ম্যাপ  </div>
+                                    <div class="text-nowrap detail-parts-font">
+                                        250 <span style="font-size: 20px;font-weight: 600;">+</span>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+
+                    </div>
+
+                </div>
+
+                @if(true)
+                    <div class="teachers mt-5">
+                        <h5 class="bold-header">শিক্ষকবৃন্দ</h5>
+                        <div class="d-flex overflow-x-scroll" style="border-radius: 25px 25px 0 0; background-color: #eeeeee; padding: 10px">
+                            {{-- @foreach($category->teacher_lists as $teacher)
+                                <div class="text-center col-6 d-flex flex-column justify-content-center align-items-center" style="padding: 0 10px; align-content: center; border-right: 1px solid lightgrey;height: 175px">
+                                    @if($teacher->image)
+                                        <img style="border-radius: 50%" height="80" width="80" src="{{$teacher->image}}" alt="">
+                                    @else
+                                        <img height="80" width="80" src="/img/category_details/teacher.png" alt="">
+                                    @endif
+                                    <span><b>{{$teacher->name}}</b></span>
+                                        <span>A great teacher of all</span>
+                                </div>
+                            @endforeach --}} 
+                        </div>
+                    </div>
+                @endif
+
+                <div class="exam-details mt-5">
+                    <nav>
+                        <div class="nav custom-shadow nav-tabs d-flex justify-content-center align-items-center" id="nav-tab" role="tablist">
+                            <a class="nav-link active"
+                               data-toggle="tab"
+                               href="#examDetails"
+                               role="tab"
+                               aria-controls="examDetails"
+                               aria-selected="true"
+                               style="border-top-right-radius: 0px !important; padding:15px">এক্সাম বিস্তারিত</a>
+
+                            <a class="nav-link"
+                               data-toggle="tab"
+                               href="#examRoutine"
+                               role="tab"
+                               aria-controls="examRoutine"
+                               aria-selected="false"
+                               style="border-top-left-radius: 0px !important; padding: 15px">রুটিন</a>
+
+                        </div>
+                    </nav>
+                    <div style="border: 5px solid #eeeeee; border-radius: 15px" class="tab-content mt-5" id="nav-tabContent">
+                        <div style="padding: 20px;overflow-y:scroll;word-break: break-word;" class="tab-pane text-justify col-md-12 fade active show"
+                             id="examDetails"
+                             role="tabpanel"
+                             aria-labelledby="video-tab">
+                            @if(true)
+                                {{-- {!! $category->details !!} --}}
+                                detail part
+                            @else
+                                <div class="text-center">
+                                    <img style="width: 100%; height: 100%" src="/img/category_details/exam_details.svg"/>
+                                    <span style="color: grey">এক্সামের বিস্তারিত এখানে দেয়া হবে</span>
+                                </div>
+                            @endif
+
+                        </div>
+                        <div style="padding: 20px;overflow-y:scroll;word-break: break-word;" class="tab-pane text-justify col-md-12 fade"
+                             id="examRoutine"
+                             role="tabpanel"
+                             aria-labelledby="examRoutine-tab">
+                            @if(true)
+                                <img class="img-fluid" src="" alt="">
+                            @else
+                                <div class="text-center">
+                                    <img style="width: 100%; height: 100%" src="/img/category_details/exam_routine.svg"/>
+                                    <span style="color: grey">এক্সামের রুটিন এখানে দেয়া হবে</span>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+
+                <div class="faq mt-5">
+                    <h5 class="bold-header">সচারচর জিজ্ঞাসা</h5>
+                    <div class="accordion mt-5" id="accordionExample">
+                        <div class="tabtab">
+                            <div style="background-color: #eeeeee; border-radius: 15px; padding: 10px;box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);">
+                                <h2 class="mb-0">
+                                    <button style="color: #6400c8; font-weight: bold;text-shadow: 2px 1px 4px white;"
+                                            class="btn btn-block text-left panel-heading"
+                                            type="button"
+                                            data-toggle="collapse"
+                                            data-target="#itemOne"
+                                            aria-expanded="true"
+                                            id="headingOne"
+                                            aria-controls="itemOne">
+                                        কোনো টেকনিক্যাল সমস্যা কিভাবে রিপোর্ট করবো?
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="itemOne"
+                                 class="panel-collapse collapse mt-2 one"
+                                 aria-labelledby="headingOne">
+                                পাখিরাই সাধারণত কীট-পতঙ্গের প্রধান শত্রু। পাখি এবং অন্যান্য শত্রুদের আক্রমণ এড়াবার জন্যে কীট-পতঙ্গ জাতীয় প্রাণীদের মধ্যে  অপেক্ষাকৃত উন্নত শ্রেণীর প্রাণী অপেক্ষা বহুল পরিমাণে অনুকরণপ্রিয়তা পরিলক্ষিত হয়।
+                            </div>
+                        </div>
+
+                        <div class="tabtab">
+                            <div class="mt-4" style="background-color: #eeeeee; border-radius: 15px; padding: 10px;box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);">
+                                <h2 class="mb-0">
+                                    <button
+                                        style="color: #6400c8; font-weight: bold; text-shadow: 2px 1px 4px white;"
+                                        class="btn btn-block text-left panel-heading"
+                                        type="button"
+                                        data-toggle="collapse"
+                                        data-target="#itemTwo"
+                                        aria-expanded="true"
+                                        id="headingTwo"
+                                        aria-controls="itemTwo">
+                                        কোর্সটি কিভাবে কিনবো?
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="itemTwo"
+                                 class="panel-collapse collapse mt-2 two"
+                                 aria-labelledby="headingTwo">
+                                পাখিরাই সাধারণত কীট-পতঙ্গের প্রধান শত্রু। পাখি এবং অন্যান্য শত্রুদের আক্রমণ এড়াবার জন্যে কীট-পতঙ্গ জাতীয় প্রাণীদের মধ্যে  অপেক্ষাকৃত উন্নত শ্রেণীর প্রাণী অপেক্ষা বহুল পরিমাণে অনুকরণপ্রিয়তা পরিলক্ষিত হয়।
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-100 ml-lg-5 mt-5">
+                <div class="test">
+                    <iframe
+                        id="iframe"
+                        src=""
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen>
+                    </iframe>
+
+
+                    <div class="payment-btn text-center">
+                        @php($href = auth()->check() ? '/' : 'javascript:void(0)')
+                        @php($loginAlert = auth()->check() ? '' : 'loginAlert')
+                        
+                        <div id="payment_section">
+                            @if(true)
+                                <div class="d-flex justify-content-around">
+                                    <span style="text-decoration: line-through; color: red" class="actual-price">250</span>
+
+                                    <span style="" class="offer-price">20</span>
+                                </div>
+                                <a class="{{$loginAlert}} btn category-details-action-btn" href="{{$href}}">কোর্সটি কিনুন</a>
+
+                            @elseif(true)
+                                <div id="priceCalculation" class="justify-content-center d-flex align-items-center">
+                                    <span class="actual-price">category price</span>
+                                    <span class="discount-amount"></span>
+                                </div>
+
+                                <a id="examPurchase" class="{{$loginAlert}} btn category-details-action-btn" href="{{$href}}">এক্সামটি কিনুন</a>
+
+                            @else
+                                <a class="btn category-details-action-btn" href="/">পরীক্ষার জন্য যান</a>
+                            @endif
+                        </div>
+
+                    </div>
+
+                    <div class="payment-info text-center mt-2">
+                        <span style="font-size: 13px; color: grey">কোর্সটি কিনুন, বাটনটিতে ক্লিক করলে আপনার পেমেন্ট প্রক্রিয়া শুরু হয়ে যাবে। </span><br>
+                        <span style="font-size: 13px; color: grey">পেমেন্ট প্রক্রিয়া বিস্তারিত জানতে এই </span> <a style="text-decoration: none; font-size: 13px; margin-left: 2px" href="/">ভিডিও দেখুন </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
 </x-landing-layout>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.9/sweetalert2.all.min.js" integrity="sha512-IZ95TbsPTDl3eT5GwqTJH/14xZ2feLEGJRbII6bRKtE/HC6x3N4cHye7yyikadgAsuiddCY2+6gMntpVHL1gHw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+   
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 300){
+            $('.payment-btn').addClass('sticky')
+        } else{
+            $('.payment-btn').removeClass('sticky')
+        }
+    });
+    $('.loginAlert').on('click', function () {
+        Swal.fire({
+            icon: 'info',
+            title: 'Please login to continue',
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
+    })
+   
+
+
+    $('.one').on('show.bs.collapse', function () {
+        $('#headingOne').addClass('active');
+    });
+
+    $('.one').on('hide.bs.collapse', function () {
+        $('#headingOne').removeClass('active');
+    });
+
+    $('.two').on('show.bs.collapse', function () {
+        $('#headingTwo').addClass('active');
+    });
+
+    $('.two').on('hide.bs.collapse', function () {
+        $('#headingTwo').removeClass('active');
+    });
+</script>
